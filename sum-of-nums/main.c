cc21716ab55aa2b9b3edb2e345ca7879bebb2c4d
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-07-06 15:20:58 +0530

    feat: added sum of n numbers in c

---
+++ sum-of-nums/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  int n, sum = 0;
+
+  printf("Enter a positive integer\n");
+
+  scanf("%d", &n);
+
+  for (int i = 1; i <= n; ++i) {
+    sum += i;
+  }
+
+  printf("Sum of natural numbers from 1 to % d : % d\n", n, sum);
+
+  return 0;
+}
